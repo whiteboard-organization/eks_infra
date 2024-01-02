84f8e0264b9d7792cd554565b9ad6db6b6893a03
ahmedMZ <mzoughi.ahmed94@gmail.com>
2024-01-02 22:26:11 +0100

    basic vpc deployment with 3 private subnets & 3 public subnets on 3 different azs

--- network-infra/main.tf
+++ network-infra/main.tf
@@ -1,4 +1,18 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "simply-network-testing"  # just testing again
-  acl    = "private"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name                 = "whiteboard-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
