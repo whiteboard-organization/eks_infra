5f22284658924810601d48ffa1600e9c7b3089d6
Oumayma AMRI <oumayma.amri-ext@kering.com>
2024-11-27 11:07:25 +0100

    feat: update eks cluster infra

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -36,7 +36,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["t3.micro"]
+    instance_types = ["t3.small"]
 
     attach_cluster_primary_security_group = true
   }
@@ -44,10 +44,10 @@ module "eks" {
   eks_managed_node_groups = {
     whiteboard-cluster-wg = {
       min_size     = 1
-      max_size     = 2
-      desired_size = 2
+      max_size     = 3
+      desired_size = 3
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.small"]
       capacity_type  = "SPOT"
 
       tags = {
